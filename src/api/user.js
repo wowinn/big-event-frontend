1528d0e83d4f8c4be051492ce2951ff9efc47eaf
wowinn <gedouju@163.com>
2024-06-07 11:39:20 +0800

    用户头像回显

--- src/api/user.js
+++ src/api/user.js
@@ -24,3 +24,8 @@ export const userLoginService = (loginData) => {
 export const userInfoService = () => {
     return request.get('/user/userInfo')
 }
+
+//修改个人信息
+export const userInfoUpdateService = (userInfoData) => {
+    return request.put('/user/update', userInfoData)
+}
